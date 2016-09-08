9a0ee9ca4c8bd660bbd3e85eb482ac519db7a475
Zhirayr K <quicksilverwork@gmail.com>
2016-09-08 04:43:20 +0400

    Update person.rb

--- lib/pipedrive/person.rb
+++ lib/pipedrive/person.rb
@@ -2,11 +2,13 @@ module Pipedrive
   class Person < Base
 
     class << self
+      def find_or_create_by_email(email, opts={})
+        find_by_name(email, :search_by_email => 1, :org_id => opts[:org_id]).first || create(opts.merge(:email => email))
+      end
 
       def find_or_create_by_name(name, opts={})
         find_by_name(name, :org_id => opts[:org_id]).first || create(opts.merge(:name => name))
       end
-
     end
 
     def deals()
